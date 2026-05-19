a6bd3460389aeb85c84cda4dfa28e00d8c396806
lordiife <osamaomarhasan07@gmail.com>
2026-05-19 15:37:44 +0300

    Remove comments and clean up projectAI.pl

--- projectAI.pl
+++ projectAI.pl
@@ -235,6 +235,7 @@ collect_eligible_majors(Field, School, Grade, Acc, [Major|Majors]) :-
     Grade >= Min,
     \+ member(Major, Acc),
     collect_eligible_majors(Field, School, Grade, [Major|Acc], Majors).
+
 collect_eligible_majors(Field, School, Grade, Acc, []) :-
     \+ ( major(Field, School, Major, Min),
          Grade >= Min,
